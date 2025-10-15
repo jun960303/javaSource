547a745dc8982e855c8768a7de03277828b662fb
jun960303 <rhkddnjs7878@naver.com>
2025-10-15 13:18:48 +0900

    String클래스

---
+++ ch3/src/lang/StringEx3.java
@@ -0,0 +1,12 @@
+package lang;
+
+public class StringEx3 {
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+
+        // 문자열 거꾸로 출력하기
+        for (int i = str1.length() - 1; i >= 0; i--) {
+            System.out.println(str1.charAt(i));
+        }
+    }
+}
